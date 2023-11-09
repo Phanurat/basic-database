a3ca55788530da0228b39002345b5282012cf4af
Phanurat <paunavt5@gmail.com>
2023-11-09 11:14:46 +0700

    Show Data to Table By Boostrap5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,35 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Hello Laravel 8
+            <!--user by Auth to user => name -->
+            Welcome, {{Auth::user()->name}}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{$users}}
+        <div class="container">
+            <div class="row">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Date Login</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <!--Foreach Loop data From User-->
+                        @php ($i=1)
+                        @foreach($users as $row)    
+                        <tr>
+                            <th>{{$i++}}</th>
+                            <td>{{$row->name}}</td>
+                            <td>{{$row->email}}</td>
+                            <td>{{$row->created_at}}</td>
+                        </tr>
+                        @endforeach
+                  </table>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
